68072b378477265876bb7d77bbd6b08b55136f98
Matt <matt579777@gmail.com>
2023-09-10 01:50:00 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.cpp
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.cpp
@@ -4,15 +4,8 @@
 using namespace std;
 
 int solution(int a, int b) {
-    string ab = to_string(a) + to_string(b);
-    string ba = to_string(b) + to_string(a);
+    int ab = stoi(to_string(a) + to_string(b));
+    int ba = stoi(to_string(b) + to_string(a));
     
-    if (stoi(ab)>stoi(ba))
-    {
-        return stoi(ab);
-    }
-    else
-    {
-        return stoi(ba);
-    }
+    return (ab>ba)? ab: ba;
 }
