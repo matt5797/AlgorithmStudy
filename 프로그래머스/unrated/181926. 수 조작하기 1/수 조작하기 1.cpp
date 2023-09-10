a3a930c9aa410ff3a924f427a84a5f0a445ce3fb
Matt <matt579777@gmail.com>
2023-09-11 00:34:13 +0900

    [unrated] Title: 수 조작하기 1, Time: 0.17 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, string control) {
+    for (char c: control)
+    {
+        if (c=='w') n += 1;
+        else if (c=='s') n -= 1;
+        else if (c=='d') n += 10;
+        else if (c=='a') n -= 10;
+    }
+    
+    return n;
+}
