a551d808259e69ae8317305e8636c986860c7199
Matt <matt579777@gmail.com>
2023-09-11 01:22:48 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    
+    while(n!=1)
+    {
+        answer.push_back(n);
+        n = (n%2==0)? n/2: 3*n+1;
+    }
+    answer.push_back(n);
+    
+    return answer;
+}
