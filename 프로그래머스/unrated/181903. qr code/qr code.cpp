4f2147d28ee3851a662734207a35ac131809044b
Matt <matt579777@gmail.com>
2023-09-14 00:46:49 +0900

    [unrated] Title: qr code, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181903. qr code/qr code.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int q, int r, string code) {
+    string answer = "";
+    for (int i=r; i<code.size(); i+=q)
+        answer += code[i];
+    return answer;
+}
