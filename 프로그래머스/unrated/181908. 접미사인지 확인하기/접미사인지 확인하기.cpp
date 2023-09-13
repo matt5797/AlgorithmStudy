02d17e12b75dd252339e2bf32c56f8cc85d52fe4
Matt <matt579777@gmail.com>
2023-09-14 00:27:29 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int solution(string my_string, string is_suffix) {
     for (int i=0; i<my_string.size(); i++)
-        if (is_suffix.compare(my_string.substr(i))==0)
+        if (is_suffix == my_string.substr(i))
             return 1;
     return 0;
 }
