e1e76543067d2444b6ca393f03ee5b98bb61625f
Matt <matt579777@gmail.com>
2023-09-14 00:34:40 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
@@ -4,8 +4,7 @@
 using namespace std;
 
 int solution(string my_string, string is_prefix) {
-    for (int i=my_string.size(); i>0; i--)
-        if (my_string.substr(0, i) == is_prefix)
-            return 1;
+    if (my_string.substr(0, is_prefix.size()) == is_prefix)
+        return 1;
     return 0;
 }
