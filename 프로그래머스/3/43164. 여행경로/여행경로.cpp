d3aa2ceec30300b821cf09c49fd8261e6870c2d8
Matt <matt579777@gmail.com>
2024-02-23 21:36:52 +0900

    [level 3] Title: 여행경로, Time: 0.04 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -22,16 +22,10 @@ void DFS(vector<vector<string>> tickets, vector<string> route, vector<string>& r
     }
 }
 
-bool compare(vector<string> a, vector<string> b) {
-    if (a[0] == b[0])
-        return a[1] < b[1];
-    return a[0] < b[0];
-}
-
 vector<string> solution(vector<vector<string>> tickets) {
     vector<string> answer = {"ICN"};
     
-    sort(tickets.begin(), tickets.end(), compare);
+    sort(tickets.begin(), tickets.end());
     
     for (int i=0;i<tickets.size();i++) {
         cout << tickets[i][0] << ", " << tickets[i][1] << endl;
